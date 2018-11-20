9e18e652d1a0ca5247b46f53d7aad1cddf785312
Jacob Cedar <jcedar8640@bths.edu>
2018-11-20 11:33:32 -0500

    added merge sort stuff

--- src/com/company/inPlaceSort.java
+++ src/com/company/inPlaceSort.java
@@ -59,7 +59,30 @@ public class inPlaceSort {
         }
     }
     public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
 
+        while(i <= mid && j <= right){
+            if(arr[i] < arr[i]){
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid){
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j <= right){
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        for(k = left; k <= right; k++){
+            arr[k] = temp[k]
+        }
     }
 }
 
